5b0a8bec631a498dcf14aed20822836a70a63841
ilya-shevtsov <Dayzilya@gmail.com>
2020-12-13 20:06:55 +0300

    Sum works

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,8 +9,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var buttonOne: Button
     private lateinit var buttonTwo: Button
+    private lateinit var buttonSum: Button
+    private lateinit var buttonCalculate: Button
     private lateinit var result: TextView
-    private var startResult = "0"
+    private var startResult = " "
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,23 +20,43 @@ class MainActivity : AppCompatActivity() {
 
         buttonOne = findViewById(R.id.One)
         buttonTwo = findViewById(R.id.Two)
+        buttonSum = findViewById(R.id.Sum)
+        buttonCalculate = findViewById(R.id.Calculate)
         result = findViewById(R.id.ResultID)
         result.text = startResult
 
-        result.setOnClickListener{
-            startResult = "0"
-            result.text = "0"
+        result.setOnClickListener {
+            startResult = " "
+            result.text = " "
         }
 
-        buttonOne.setOnClickListener{
+        buttonOne.setOnClickListener {
             startResult += getString(R.string.One)
             result.text = startResult
         }
 
-        buttonTwo.setOnClickListener{
+        buttonTwo.setOnClickListener {
             startResult += getString(R.string.Two)
             result.text = startResult
         }
 
+        buttonSum.setOnClickListener {
+            startResult += getString(R.string.Sum)
+            result.text = startResult
+        }
+
+        buttonCalculate.setOnClickListener {
+            result.text = calculate()
+        }
+
+
+    }
+
+    private fun calculate(): String {
+        val split = startResult.split("+")
+        val numbers = split.map { numberString ->
+            numberString.replace(" ","").toInt()
+        }
+        return numbers.sum().toString()
     }
 }
